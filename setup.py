364bad3c5180dbf95b2972ebf8c2e5d358741e1c
Joe Paavola <joe@workhere.com>
2020-09-14 15:42:36 -0400

    Updated version and gitignore

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="drf-spreadsheets",
-    version="1.0.0",
+    version="1.0.1",
     author="Joe Paavola",
     author_email="joepaavola@gmail.com",
     description="A Django REST Framework package allowing views to be rendered as CSV or XLSX",
