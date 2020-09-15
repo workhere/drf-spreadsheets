42fcec029b753902f6cf9d1f13232d7e7e9731d6
Joe Paavola <joe@workhere.com>
2020-09-15 09:27:43 -0400

    Added some small features: - enable/disable on detail views for ViewSets - enable/disable default renderers for spreadsheet-only endpoints - auto-filenaming that works without a model field on the view
    Also added better mixin documentation

--- drf_spreadsheets/mixins.py
+++ drf_spreadsheets/mixins.py
@@ -6,22 +6,40 @@ from drf_spreadsheets.renderers import CSVRenderer, XLSXRenderer, SpreadsheetRen
 
 class SpreadsheetMixIn:
     """
-    A MixIn that allows a viewset to serialize list data as CSV or Excel
+    A MixIn that allows a REST view to serialize list data as CSV or Excel
     """
 
+    # If set to True, the CSVRenderer will be made available in the view this MixIn is added to
     enable_csv = True
 
+    # If set to True, the XLSXRenderer will be made available in the view this MixIn is added to
     enable_xlsx = True
 
+    # If set to True, the renderers provided by this MixIn will be available on views marked as "details".
+    # This is particularly useful for ViewSets, where the list view should have spreadsheet capabilities,
+    # but the single record detail should not.
+    enable_on_detail = False
+
+    # If set to True, the default renderers found in api_settings.DEFAULT_RENDERER_CLASSES will be preserved as options
+    # for the view this MixIn is added to. Setting this to false should only be done for spreadsheet only endpoints.
+    enable_defaults = True
+
+    # Setting filename will override the default naming system, which take the model or viewset name and appends
+    # ' Report' and the file extension. You do not need to include the file extension in this parameter, it will be
+    # automatically resolved for you.
     filename = None
 
     def __init__(self, *args, **kwargs):
-        classes = []
-        if self.enable_csv and CSVRenderer not in self.renderer_classes:
-            classes.append(CSVRenderer)
-        if self.enable_xlsx and XLSXRenderer not in self.renderer_classes:
-            classes.append(XLSXRenderer)
-        self.renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES + classes
+        if not kwargs["detail"] or self.enable_on_detail:
+            classes = []
+            if self.enable_csv:
+                classes.append(CSVRenderer)
+            if self.enable_xlsx:
+                classes.append(XLSXRenderer)
+            if self.enable_defaults:
+                self.renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES + classes
+            else:
+                self.renderer_classes = classes
         super(SpreadsheetMixIn, self).__init__()
 
     def paginate_queryset(self, queryset):
@@ -41,16 +59,21 @@ class SpreadsheetMixIn:
         response = super(SpreadsheetMixIn, self).finalize_response(
             request, response, *args, **kwargs
         )
-        filename = (
-            self.filename
-            if self.filename is not None
-            else f"{self.model.__name__} Report"
-        )
+
         if isinstance(response, Response):
+            # Resolve filename, either from override, model, or view name
+            if self.filename is not None:
+                filename = self.filename
+            elif self.model is not None:
+                filename = f"{self.model.__name__} Report"
+            else:
+                filename = f"{self.get_view_name()} Report"
+
+            # Only add Content-Disposition if the renderer is XLSX or CSV
             if response.accepted_renderer.format == "xlsx":
                 response[
-                    "content-disposition"
+                    "Content-Disposition"
                 ] = f"attachment; filename={filename}.xlsx"
             elif response.accepted_renderer.format == "csv":
-                response["content-disposition"] = f"attachment; filename={filename}.csv"
+                response["Content-Disposition"] = f"attachment; filename={filename}.csv"
         return response
